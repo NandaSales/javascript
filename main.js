57e366828e5f1343b94dfab78d83fba826a587fd
NandaSales <frs17@aluno.ifnmg.edu.br>
2025-08-05 19:07:41 +0000

    n

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 console.log('Olá mundo!'); 
-novoElemento.style.backgroundColor = 'blue';
-novoElemento.style.color = 'yellow';
+// Criando um novo elemento 
+let novoElemento = document.createElement('h1');
+// Alterando o conteúdo de texto do elemento
+novoElemento.innerText = 'Hello, World! English! (Inglês) Ok?!';
+// Selecionando o elemento body
+let elementoBody = document.body;
+// Colocando o novo elemento no body
+elementoBody.appendChild(novoElemento);
